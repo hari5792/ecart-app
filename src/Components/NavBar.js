1014be245a7d134f148e2c34a66bc4947c7888c7
hari5792 <hari.1901047@srec.ac.in>
2023-08-15 17:11:15 +0530

    NavBar added

---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './NavBar.css'
+function NavBar ()
+{
+    return (
+    <nav class="myNav">                                 
+    <ul>
+        <li><a href="www.google.com">Google</a></li>
+        <li><a href="https://www.youtube.com/">YouTube</a></li>
+        <li><a href="instagram.com">Instagram</a></li>
+    </ul>
+    </nav>
+    );
+}
+
+export default NavBar
